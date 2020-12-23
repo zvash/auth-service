a332006bc9f59d1b5e0989c90fb7e24b4bf152ab
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2020-12-23 10:00:40 +0330

    list of countries is limited

---
+++ config/active_countries.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    "Kuwait" => [
+        "alpha2" => "KW",
+        "phone" => "+965",
+        "emoji" => "🇰🇼",
+        "alpha3" => "KWT",
+        "name" => "Kuwait",
+        "currency" => "KWD"
+    ],
+    "Qatar" => [
+        "alpha2" => "QA",
+        "phone" => "+974",
+        "emoji" => "🇶🇦",
+        "alpha3" => "QAT",
+        "name" => "Qatar",
+        "currency" => "QAR"
+    ],
+    "United Arab Emirates" => [
+        "alpha2" => "AE",
+        "phone" => "+971",
+        "emoji" => "🇦🇪",
+        "alpha3" => "ARE",
+        "name" => "United Arab Emirates",
+        "currency" => "AED"
+    ],
+    "Saudi Arabia" => [
+        "alpha2" => "SA",
+        "phone" => "+966",
+        "emoji" => "🇸🇦",
+        "alpha3" => "SAU",
+        "name" => "Saudi Arabia",
+        "currency" => "SAR"
+    ],
+    "Oman" => [
+        "alpha2" => "OM",
+        "phone" => "+968",
+        "emoji" => "🇴🇲",
+        "alpha3" => "OMN",
+        "name" => "Oman",
+        "currency" => "OMR"
+    ],
+];
